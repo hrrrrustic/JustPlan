d5dc03b98062d4668aa647d87070b369425f4764
Vlad <hrrrrustic@gmail.com>
2022-07-16 21:20:35 +0300

    Init Schedule proj

---
+++ JustPlan.Schedule/EventTypes.fs
@@ -0,0 +1,28 @@
+﻿module JustPlan.Calendar.Types
+
+open System
+
+type EventTime = {
+    Start: DateTime
+    End: DateTime
+}
+
+type EventInformation = {
+    Title: string
+    Description: string option
+}
+
+type SingleEvent = {
+    Time: EventTime
+    Information: EventInformation
+}
+
+type RepeatableEvent = {
+    InitialTime: EventTime
+    Information: EventInformation
+    RepeatDelay: TimeSpan
+}
+
+type Event =
+    | Single of SingleEvent
+    | Repeatable of RepeatableEvent
