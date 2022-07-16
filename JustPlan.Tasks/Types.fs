ea0e5ddbd74a6dff737f2e65d206c763eb958455
Vlad <hrrrrustic@gmail.com>
2022-07-16 21:20:19 +0300

    init Task proj

---
+++ JustPlan.Tasks/Types.fs
@@ -0,0 +1,19 @@
+﻿module JustPlan.Tasks.Types
+
+open JustPlan.User.Types
+
+type ToDo = {
+    Id: int
+    Title: string
+    Description: string option
+    SubToDo: ToDo array option
+}
+
+type Assignment = {
+    ToDo: ToDo
+    Assignee: User
+}
+
+type WorkItem =
+    | Assignment of Assignment
+    | ToDo of ToDo
